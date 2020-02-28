a157b0c31fdd5e2e66529811023bcd89153b1fdf
mwejuli <mwejuli@companieshouse.gov.uk>
2020-02-28 16:10:54 +0000

    chore: remove extraneous comments from the router

--- server/router.js
+++ server/router.js
@@ -3,19 +3,4 @@
 
 module.exports = function (app) {
   app.use('/', require('./routes/report'));
-  // app.use('/report-a-discrepancy', require('./routes/discrepancy'));
-  // app.use('/report-a-discrepancy/obliged-entity/name', require('./routes/discrepancy'));
-  // app.use('/report-a-discrepancy/obliged-entity/email', require('./routes/discrepancy'));
-  // app.use('/report-a-discrepancy/psc/company-number', require('./routes/discrepancy'));
-  // app.use('/report-a-discrepancy/psc/discrepancies', require('./routes/discrepancy'));
-  // app.use('/report-a-discrepancy/confirmation', require('./routes/discrepancy'));
-
 }
-
-/*
-
-- rename to report
-- constify long strings
--
-
-*/
