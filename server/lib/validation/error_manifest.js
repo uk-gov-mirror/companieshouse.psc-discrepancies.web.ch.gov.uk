368eaa808e762a3aef4512c3360961a199778b9e
mreis-ch <mreis@companieshouse.gov.uk>
2020-03-13 11:32:28 +0000

    Add validation to discrepancy details screen
    Add new method to validation/index.js that validates the discrepancy details is populated and containts alphabetic characters
    Add post endpoint for discrepancy details to report.js
    Add error classes to deiscrepancy_details.njk
    Add unit tests
    
    FAML-341

--- server/lib/validation/error_manifest.js
+++ server/lib/validation/error_manifest.js
@@ -1,7 +1,11 @@
 let ErrorManifest = {
   email: {
     summary: "Enter your email address",
-    inline: "Please enter a correct email"
+    inline: "Enter your email address"
+  },
+  details: {
+    summary: "Enter the information that is incorrect for the PSC",
+    inline: "Enter the information that is incorrect for the PSC"
   }
 }
 
