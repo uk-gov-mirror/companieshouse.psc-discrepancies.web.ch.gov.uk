3234704dbcb463e99f49a0f4b6381396851fb071
mreis-ch <mreis@companieshouse.gov.uk>
2020-03-26 10:21:11 +0000

    Add session handler
    Add two test endpoints, one to save  and one to load from redis and store a temporary value

--- server/routes/report.js
+++ server/routes/report.js
@@ -1,8 +1,43 @@
 const router = require('express').Router();
 const routeViews = 'report';
 const Validator = require(`${serverRoot}/lib/validation`);
+const session = require('ch-node-session-handler');
+const Redis = require("ioredis");
+const { Session, SessionStore }  = require('ch-node-session-handler');
 
 const validator = new Validator();
+const cookie = {sessionId: '__SID-value-from-browser', signature: `${process.env.COOKIE_SECRET}`};
+
+router.get('/report-a-discrepancy/session-test-1', (req, res, next) => {
+  const sessionStore = new SessionStore(new Redis(`redis://${process.env.CACHE_SERVER}`));
+  sessionStore.load(cookie).run()
+    .then(data => {
+      const session = new Session(data.extract());
+      session.saveExtraData('xyz987', 'xyz987Value');
+      console.log('1st sess');
+      console.log(session);
+      
+      sessionStore.store(cookie, session.data).run()
+        .then(d => {
+          res.set('Content-Type', 'application/json');
+          return res.status(200).json(session);
+        });
+    });
+});
+
+router.get('/report-a-discrepancy/session-test-2', (req, res, next) => {
+  const sessionStore = new SessionStore(new Redis(`redis://${process.env.CACHE_SERVER}`));
+  sessionStore.load(cookie).run()
+  .then(data => {
+    const session = new Session(data.extract());
+    res.set('Content-Type', 'application/json');
+    let abc = session.getExtraData('xyz987');
+    res.status(200).json({
+      extra: abc,
+      session: session
+    });
+  });
+});
 
 router.get('(/report-a-discrepancy)?', (req, res, next) => {
   res.render(`${routeViews}/index.njk`);
