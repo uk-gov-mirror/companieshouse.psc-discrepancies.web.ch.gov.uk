fd438ba3bf4e0867ac388bfbfd45d7c770c103e9
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-08-25 12:50:24 +0100

    feature: add extra field for obligedEntityType

--- server/services/data/oe_types.js
+++ server/services/data/oe_types.js
@@ -1,15 +1,16 @@
-const ObligedEntityTypes = {
+const obligedEntityTypes = {
   credit: "Credit institution",
   financial: "Financial institution",
-  accountant: "Auditor, insolvency practitioner, external accountant or tax advisor",
+  accountant: "Auditor, external accountant or tax advisor",
   notary_legal: "Notary or independent legal professional",
   estate: "Estate agent or intermediary",
-  sole_trader: "Entity trading goods in cash over €10,000",
+  cash_trader: "Entity trading goods in cash over €10,000",
   gambling: "Gambling service provider",
   foreign_exchange: "Exchange service provider of fiat and virtual currencies",
   wallet_provider: "Custodian wallet provider",
   art_dealer: "Art dealer - galleries and auction houses",
-  art_dealer_freeport: "Art dealer - free ports"
+  art_dealer_freeport: "Art dealer - free ports",
+  insolvency: "Insolvency practitioner"
 }
 
-module.exports = ObligedEntityTypes;
+module.exports = obligedEntityTypes;
