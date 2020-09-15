4fb2b745d59baa596134126e0ad60f58b661a219
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-09-15 12:11:07 +0100

    refactor: use process exception to handle error and render error page

--- server/routes/utils/index.js
+++ server/routes/utils/index.js
@@ -7,16 +7,15 @@ const Utility = require(`${serverRoot}/lib/Utility`);
 const errorManifest = require(`${serverRoot}/lib/errors/error_manifest`).generic;
 
 const routeUtils = {
-  processException: (err, res) => {
+  processException: (err, viewData, res) => {
     Utility.logException(err);
     let e = {};
     if (typeof err.code !== 'undefined' && err.code === 'VALIDATION_ERRORS') {
-      e = err.stack;
+      viewData.this_errors = err.stack;
+      res.render(viewData.path, viewData);
     } else {
-      return res.render('_partials/error.njk');
-      //e.genericError = errorManifest.serverError;
+      res.render('_partials/error.njk');
     }
-    return e;
   }
 }
 
