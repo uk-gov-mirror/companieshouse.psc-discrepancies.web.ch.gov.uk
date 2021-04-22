a46467e164cab9153184efc7b193dad5b8048820
Iain Craig <>
2021-04-22 08:08:08 +0100

    -add cookie-consent-banner.njk -upgraded govuk-frontend up to 3.11.0 -added chsurl to be able to access cookie help page -updated default.njk to include cookie-consent-banner.njk

--- server/app.js
+++ server/app.js
@@ -62,6 +62,7 @@ app.use((err, req, res, next) => {
 njk.addGlobal('cdnUrlCss', process.env.CDN_URL_CSS);
 njk.addGlobal('cdnUrlJs', process.env.CDN_URL_JS);
 njk.addGlobal('cdnHost', process.env.CDN_HOST);
+njk.addGlobal('chsUrl', process.env.CHS_URL);
 njk.addGlobal('piwikUrl', process.env.PIWIK_URL);
 njk.addGlobal('piwikSiteId', process.env.PIWIK_SITE_ID);
 njk.addGlobal('discrepancyGoalId', process.env.DISCREPANCIES_PIWIK_START_GOAL_ID);
